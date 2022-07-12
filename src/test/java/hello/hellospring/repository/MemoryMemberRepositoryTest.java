846e5871015eb9a63e432f803e5ea87ce7011ced
whistleework <whistlee45@gmail.com>
2022-07-12 10:38:43 +0900

    Changes to be committed: 	new file:   .gitignore 	new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/main/java/hello/hellospring/HelloSpringApplication.java 	new file:   src/main/java/hello/hellospring/controller/HelloController.java 	new file:   src/main/java/hello/hellospring/domain/Member.java 	new file:   src/main/java/hello/hellospring/repository/MemberRepository.java new file:   src/main/java/hello/hellospring/repository/MemoryMemberRepository.java 	new file:   src/main/resources/application.properties new file:   src/main/resources/static/hello-static.html 	new file:   src/main/resources/static/index.html 	new file:   src/main/resources/templates/hello-template.html 	new file:   src/main/resources/templates/hello.html 	new file:   src/test/java/hello/hellospring/HelloSpringApplicationTests.java 	new file:   src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,42 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        //System.out.println("result = " + (result == member));
+        //Assertions.assertEquals(member, result);
+        //Assertions.assertEquals(member, null);
+
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Optional<Member> result = repository.findByName("spring1");
+    }
+}
